f0fd749f61ed62b1a04ca1b5e3102e8e6e3e92a2
Torio Yoshiko <torio.yoshiko@gmail.com>
2020-04-16 19:28:40 +0200

    Updates

---
+++ precache-manifest.54f71c8e7941e833c886e5f650798be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab51cf661841df504282b2d9fa8c7a73",
+    "url": "/github-search/index.html"
+  },
+  {
+    "revision": "0b82c0b8cbf26344fc98",
+    "url": "/github-search/static/css/main.4cf2e7d3.chunk.css"
+  },
+  {
+    "revision": "818306874e310b899d94",
+    "url": "/github-search/static/js/2.d3820ad5.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/github-search/static/js/2.d3820ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b82c0b8cbf26344fc98",
+    "url": "/github-search/static/js/main.3a464f14.chunk.js"
+  },
+  {
+    "revision": "09cfeb4ff2540d50b1d9",
+    "url": "/github-search/static/js/runtime-main.d1e48445.js"
+  }
+]);
